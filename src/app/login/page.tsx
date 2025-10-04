235b9afe048bd6cd3437b9c46129a1bd36439c4c
Mohit kumar <cooldudemohit66@gmail.com>
2025-10-04 12:24:33 +0000

    much more smaller

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
         <CardHeader className="text-center">
             <div className="flex justify-center mb-4">
                  <Link href="/">
-                    <Image src="/VGS-logo.png" alt="Vidhi Gyan Sodh Logo" width={120} height={32} priority />
+                    <Image src="/VGS-logo.png" alt="Vidhi Gyan Sodh Logo" width={100} height={26} priority />
                  </Link>
             </div>
           <CardTitle className="text-2xl font-headline">Welcome Back</CardTitle>
