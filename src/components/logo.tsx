72943054544cc3cdb0230a19a3b3fd5e5228a7a1
Mohit kumar <cooldudemohit66@gmail.com>
2025-10-04 12:18:46 +0000

    why the image is not rendering

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Logo = ({ className }: { className?: string }) => {
   return (
     <Link href="/" className={cn("flex items-center", className)}>
-      <Image src="/logo.png" alt="Vidhi Gyan Sodh Logo" width={150} height={40} />
+      <Image src="/logo.png" alt="Vidhi Gyan Sodh Logo" width={150} height={40} priority />
     </Link>
   );
 };
